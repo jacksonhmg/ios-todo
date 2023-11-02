0484409c6f20ade2a41770f69fc8dd9089e007d7
jacksonhmg <jacksonmowattgok@gmail.com>
2023-11-02 16:13:15 -0700

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by jackson mowatt gok on 2/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
