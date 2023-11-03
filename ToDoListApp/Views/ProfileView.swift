1e6d7279636b7dc70def7127aba231bbab4a4b4a
jacksonhmg <jacksonmowattgok@gmail.com>
2023-11-02 22:41:02 -0700

    fetch user details on profile, log out working too

--- ToDoListApp/Views/ProfileView.swift
+++ ToDoListApp/Views/ProfileView.swift
@@ -13,9 +13,47 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                
+                if let user = viewModel.user {
+                    // Avatar
+                    Image(systemName: "person.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                        .frame(width: 125, height: 125)
+                    
+                    // Info: Name, Email, Member since
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text("Name: ")
+                            Text(user.name)
+                        }
+                        HStack {
+                            Text("Email: ")
+                            Text(user.email)
+                        }
+                        HStack {
+                            Text("Member Since: ")
+                            Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+                        }
+                    }
+                    .padding()
+                    
+                    // Sign out
+                    Button("Log Out") {
+                        viewModel.logOut()
+                    }
+                    .tint(.red)
+                    .padding()
+                    
+                    Spacer()
+                } else {
+                    Text("Loading Profile")
+                }
             }
             .navigationTitle("Profile")
+        }
+        .onAppear{
+            viewModel.fetchUser()
         }
     }
 }
